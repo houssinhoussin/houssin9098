159227d4dce174e92e678bd2963d58cc02d499d8
houssinhoussin <anmarhoussin1@gmail.com>
2025-09-11 02:52:32 +0100

    Update internet_providers.py

--- handlers/internet_providers.py
+++ handlers/internet_providers.py
@@ -11,6 +11,9 @@ except Exception:
 
 import logging
 import re
+from handlers.start import _reset_user_flows
+_reset_user_flows(m.from_user.id)
+
 from telebot import types
 
 from services.wallet_service import (
