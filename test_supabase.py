482dbcf2e5310051cc48070fb97dfecc87c73b53
houssinhoussin <anmarhoussin1@gmail.com>
2025-09-11 19:12:42 +0100

    Update test_supabase.py

--- test_supabase.py
+++ test_supabase.py
@@ -1,7 +1,8 @@
 from database.db import get_table
 
-TABLE_NAME = "USERS_TABLE"
+from config import SUPABASE_TABLE_NAME as TABLE_NAME
+if TABLE_NAME == "USERS_TABLE":
+    TABLE_NAME = "houssin363"
 table = get_table(TABLE_NAME)
 result = table.select("*").limit(1).execute()
 print(result.data)
-
