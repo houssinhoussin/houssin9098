c9ea5c0931a1b11f81288e7c28c874d8da1e0c6e
houssinhoussin <anmarhoussin1@gmail.com>
2025-09-11 02:54:48 +0100

    Update wholesale.py

--- handlers/wholesale.py
+++ handlers/wholesale.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # handlers/wholesale.py
+from handlers.start import _reset_user_flows
+_reset_user_flows(m.from_user.id)
 
 from telebot import types
 from config import ADMIN_MAIN_ID
